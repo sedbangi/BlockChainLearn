608841288d2d6b98576b100f40386405fca4ac37
calvin <806289038@qq.com>
2024-10-15 13:03:04 +0800

    【feature】(1.0.6-tokenBankV2)

---
+++ tokenBankV2/impl/TokenBankV2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./TokenBank.sol";
+import "../IERC20BankV2.sol";
+
+contract TokenBankV2 is TokenBank, IERC20BankV2{
+
+    //execute when receive tokens (who (userAddress) save how much ( value ) what( tokenAddress ) )
+    function tokensReceived(address userAddress, address tokenAddress, uint value) external {
+        balances[userAddress][tokenAddress] += value;
+    }
+
+}
