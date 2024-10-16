620cac2f74ab6c8d752dde2713be0703f6409b4d
Calvin <806289038@qq.com>
2024-10-16 11:07:19 +0800

    Update TokenBankV2.sol

--- tokenBankV2/impl/TokenBankV2.sol
+++ tokenBankV2/impl/TokenBankV2.sol
@@ -8,7 +8,7 @@ contract TokenBankV2 is TokenBank, IERC20BankV2{
 
     //execute when receive tokens (who (userAddress) save how much ( value ) what( tokenAddress ) )
     function tokensReceived(address userAddress, address tokenAddress, uint value) external {
-        require(msg.sender == tokenAddress, "sender address must equal to token address")
+        require(msg.sender == tokenAddress, "sender address must equal to token address");
         balances[userAddress][tokenAddress] += value;
     }
 
