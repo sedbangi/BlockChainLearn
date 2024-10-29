2669da35a0c075dc6447e72f9d513ff6effe7d78
calvin <806289038@qq.com>
2024-10-29 23:54:06 +0800

    dePermit2

--- 14_permit2AndCreate2/script/Deploy.sol
+++ 14_permit2AndCreate2/script/Deploy.sol
@@ -5,14 +5,14 @@ import "../lib/forge-std/src/Script.sol";
 import "../lib/forge-std/src/console.sol";
 import "../src/impl/CalvinERC20.sol";
 import "../src/impl/TokenBank.sol";
-import "../src/impl/Permit2Calvin.sol";
+import "../lib/permit2/src/Permit2.sol";
 
 contract Deploy is Script {
 
     function run() public {
         vm.startBroadcast(vm.envUint("privateKey"));
         CalvinERC20 token = new CalvinERC20();
-        Permit2Calvin permit2 = new Permit2Calvin();
+        Permit2 permit2 = new Permit2();
         TokenBank tokenBank = new TokenBank(address (token),address (permit2));
         vm.stopBroadcast();
     }
